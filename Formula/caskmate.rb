dce8c02a0942f7776aa609d698163655ecccd78d
Giovanni Rizzello <113061484+giovannirizzello@users.noreply.github.com>
2025-09-14 01:46:06 +0200

    Fix caskmate formula v1.0.0

--- Formula/caskmate.rb
+++ Formula/caskmate.rb
@@ -2,13 +2,13 @@ class Caskmate < Formula
   desc "CLI tool to manage apps in /Applications via Homebrew"
   homepage "https://github.com/giovannirizzello/caskmate"
   url "https://github.com/giovannirizzello/caskmate/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "e396681fd399da5fac9abe3b44e93079129ce9408513b6bd0782a21f35e65f2f"
+  sha256 "9bf14f0d5f7d51f186b761dbd70ea7dfc2797fbb542364564451b3f75d97249f"
   license "MIT"
 
   depends_on "python@3.11"
 
   def install
-    bin.install "caskmate" => "caskmate"
+    bin.install "caskmate/caskmate" => "caskmate"
   end
 
   test do
